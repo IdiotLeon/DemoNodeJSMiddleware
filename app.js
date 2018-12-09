e5f2abdbe586a986c93d40a2e0cc57de6a64f727
Leon <yanglyu.leon.7@gmail.com>
2018-12-09 14:55:05 -0800

    demo: Adding Application-level middlewares

--- app.js
+++ app.js
@@ -46,4 +46,71 @@ app.use(router)
 const configurableMiddleware = require('./configurable-middleware.js')
 app.use(configurableMiddleware({ options1: '1', options2: '2' }))
 
+/**
+ * Application-level middlewares
+ * https://expressjs.com/en/guide/using-middleware.html#middleware.application
+ * Here is an example of loading a series of middleware functions at a mount point, with a mount path. 
+ * It illustrates a middleware sub-stack that prints request info for any type of HTTP request to the /test/mwstack path.
+ */
+app.use('/test/mwstack',
+    (req, res, next) => {
+        console.log('middleware stack 01')
+        next()
+    }, (req, res, next) => {
+        console.log('middleware stack 02')
+        next()
+    }, (req, res) => {
+        res.send('Hello World from middleware stacks')
+    })
+
+
+
+/**
+ * Application-level middlewares 
+ * https://expressjs.com/en/guide/using-middleware.html#middleware.application
+ * Route handlers enable you to define multiple routes for a path. 
+ * The example below defines two routes for GET requests to the /user/:id path. 
+ * The second route will not cause any problems, 
+ * but it will never get called because the first route ends the request-response cycle.
+ */
+app.get('/test/multipleroutes', (req, res, next) => {
+    console.log('multiple routes 01')
+    next()
+}, (req, res) => {
+    res.send('Hello World from multipleroutes 01')
+})
+
+/**
+ * Application-level middlewares 
+ * https://expressjs.com/en/guide/using-middleware.html#middleware.application
+ * The second route will not cause any problems, 
+ * but it will never get called because the first route ends the request-response cycle.
+ */
+app.get('/test/multipleroutes', (req, res, next) => {
+    res.end('Hello World from multipleroutes 02')
+})
+
+
+
+/**
+ * Application-level middlewares
+ * https://expressjs.com/en/guide/using-middleware.html#middleware.application
+ * To skip the rest of the middleware functions from a router middleware stack, 
+ * call next('route') to pass control to the next route. 
+ * NOTE: next('route') will work only in middleware functions that were loaded 
+ * by using the app.METHOD() or router.METHOD() functions.
+ */
+app.get('/test/skip/:id', (req, res, next) => {
+    console.log(`req.params, middleware: ${JSON.stringify(req.params)}`)
+    if (req.params.id === '0') next('route')
+    else next()
+}, (req, res, next) => {
+    console.log(`req.params: ${JSON.stringify(req.params)}`)
+    res.send('regular')
+})
+
+app.get('/test/skip/:id', (req, res, next) => {
+    res.send('special')
+})
+
 app.listen(port, () => console.log(`App is listening on port ${port}`))
