68d500416709aa7ec016f8568e61e959b0f4b462
Leon <yanglyu.leon.7@gmail.com>
2018-12-09 13:03:40 -0800

    fea: Demoing a quick simple example of middlewares

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+const port = 7777;
+
+app.use(function (req, res, next) {
+    console.log('middleware 1 is called')
+    next()
+})
+
+app.use("/test", function (req, res, next) {
+    console.log('middleware 2 is called')
+    next()
+})
+
+app.get('/test', function (req, res, next) {
+    res.send('test test test')
+})
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
